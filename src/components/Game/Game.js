2b1b510c8798a2a109c0d57f7a0ecb0454c15c4c
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2024-06-09 20:14:01 -0700

    build basic GuessInput component with validation

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+
+import GuessInput from "../GuessInput/GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +11,43 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [turn, setTurn] = useState(1);
+  const [guesses, setGuesses] = useState([]);
+  const isFinished = turn > 6;
+
+  console.log("guesses:", guesses);
+  console.log("turn:", turn);
+
+  const handleSubmitGuess = (guess) => {
+    // disallow non-word characters, eg. numbers, hyphens, periods...
+    if (/\W\d+/.test(guess)) {
+      window.alert(
+        "Your guess may not include any numbers or special characters. Please enter a 5-letter word."
+      );
+      return;
+    }
+
+    // don't submit guesses that are less than 5 letters
+    if (guess.length < 5) {
+      window.alert("You must guess a 5-letter word.");
+      return;
+    }
+
+    if (guesses.includes(guess)) {
+      window.alert(
+        `You've already guessed ${guess}, please pick another word.`
+      );
+      return;
+    }
+
+    const newGuesses = [...guesses, guess];
+    setGuesses(newGuesses);
+    setTurn(turn + 1);
+
+    return true;
+  };
+
+  return !isFinished && <GuessInput handleSubmitGuess={handleSubmitGuess} />;
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function GuessInput({ handleSubmitGuess }) {
+  const [guess, setGuess] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    handleSubmitGuess(guess);
+    setGuess("");
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        minLength={5}
+        maxLength={5}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
